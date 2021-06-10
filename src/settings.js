a7be2ed0e7745fe18909ef91d63bd4264df154d9
zzh <834439603@qq.com>
2021-06-10 15:29:21 +0800

    初始化提交

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+  //项目标题
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  //是否固定头部
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  //显示左侧菜单logo
+  sidebarLogo: true
+}
